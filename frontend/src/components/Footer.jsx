4dfb974f8b0d62f8a2b1b7ae78cecfb9c95345ca
manojpm181 <manojpoojari1511@gmail.com>
2026-02-28 10:36:11 +0530

    Initial commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,8 +2,8 @@ const Footer = () => {
   return (
     <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
       <p>
-        &copy; {new Date().getFullYear()} DayBook | Created with ❤️ by Nilesh
-        Kumar
+        &copy; {new Date().getFullYear()} DayBook | Created  by Manoj P M
+       
       </p>
     </footer>
   );
